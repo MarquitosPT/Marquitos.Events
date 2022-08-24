20adfa1bc71f84fe8b1744b065371c141d4b2d35
Marcos Gomes <marquitospt@hotmail.com>
2022-08-24 20:07:55 +0100

    Added retries example

--- test/Marquitos.Events.Api/Consumers/WeatherForecastCreatedConsumer.cs
+++ test/Marquitos.Events.Api/Consumers/WeatherForecastCreatedConsumer.cs
@@ -5,11 +5,12 @@ namespace Marquitos.Events.Api.Consumers
 {
     public class WeatherForecastCreatedConsumer : EventConsumer<WeatherForecastCreated>
     {
-        
-
         public override async Task<bool> InitializeAsync(CancellationToken cancellationToken = default)
         {
             // TODO: Setup initial value options or get it from a database
+
+            // For example add two retry options
+            Options.Retries = new[] { 0.5, 1 }; // 30s and 1min
 
             // Simply return true for this demo
             return await Task.FromResult(true);
